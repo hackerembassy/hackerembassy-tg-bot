0e8a961ebba79a98471a44a7cc8bf8498dcfae59
tectonick <korn9509@gmail.com>
2023-01-09 01:16:57 +0400

    * Many important items are copyable now * Crypto donations * Added join command * Edited lots of texts and emojis

---
+++ data/coins/coins.js
@@ -0,0 +1,26 @@
+
+const fs = require('fs').promises;
+const path = require('path');
+
+const Coins = [
+    {fullname: "Bitcoin", shortname: "bth", address: "bc1q8d4y2hza9yeevjp7fyvndd6tc6pmt8k9jk70vf", network: "BTC", qrfile:"bth.jpg"},
+    {fullname: "Ethereum", shortname: "eth", address: "0x3Fd7976eeC03b07e28BDC8BeaD6e279CeF04170b", network: "ETH", qrfile:"eth.jpg"},
+    {fullname: "USD Coin", shortname: "usdt", address: "0x3Fd7976eeC03b07e28BDC8BeaD6e279CeF04170b", network: "ERC20", qrfile:"usdt.jpg"},
+    {fullname: "Tether", shortname: "usdc", address: "0x3Fd7976eeC03b07e28BDC8BeaD6e279CeF04170b", network: "ERC20", qrfile:"usdc.jpg"},
+]
+
+const QRBaseFolder = "./qr";
+
+function getCoinDefinition(coinname){
+    return Coins.find(c=>c.shortname === coinname);
+}
+
+async function getQR(coinname){
+    let buffer = await fs.readFile(path.join(__dirname, QRBaseFolder, getCoinDefinition(coinname).qrfile));
+
+    return buffer;
+}
+
+
+
+module.exports = {Coins, getQR, getCoinDefinition};

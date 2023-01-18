06f267e050498f064fa9f0e6c547bc8bc0150a83
Nikolay Kiselyov <n.kiselyov@itransition.com>
2023-01-19 00:36:07 +0400

    no-cors

--- api.js
+++ api.js
@@ -17,4 +17,10 @@ app.get("/status", (_, res) => {
   res.send(content);
 });
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.listen(port);

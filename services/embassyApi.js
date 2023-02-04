333c087b1f76258631b89548d497a4fd6866beac
tectonick <korn9509@gmail.com>
2023-02-05 00:34:30 +0400

    * new mac detection implementation

--- services/embassyApi.js
+++ services/embassyApi.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const config = require("config");
@@ -5,13 +6,45 @@ const printer3d = require("./printer3d");
 const embassyApiConfig = config.get("embassy-api");
 const app = express();
 const port = embassyApiConfig.port;
+const routerip = embassyApiConfig.routerip;
 const find = require('local-devices');
+const { LUCI } = require('luci-rpc');
+const fetch = require('node-fetch');
 
 app.use(cors());
 
-app.get("/devices", async (_, res) => {
+app.get("/devicesold", async (_, res) => {
   let devices = await find({ address: embassyApiConfig.networkRange });
   res.send(devices.map(d=> d.mac));
+});
+
+app.get("/devices", async (_, res) => {
+
+  const luci = new LUCI(`https://${routerip}`, 'bot', process.env["LUCITOKEN"]);
+  await luci.init();
+  luci.autoUpdateToken(1000 * 60 * 30);
+
+  let rpc =
+  [
+    {"jsonrpc":"2.0","id":28,"method":"call","params":[luci.token,"iwinfo","assoclist",{"device":"wlan0"}]},
+    {"jsonrpc":"2.0","id":29,"method":"call","params":[luci.token,"iwinfo","assoclist",{"device":"wlan1"}]}
+  ];
+
+  let response = await fetch(`http://${routerip}/ubus/`, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(rpc),
+    method: "POST"
+  });
+
+  let json = await response.json();
+  let macs = [];
+  for (const wlan of json) {
+    macs = macs.concat(wlan.result[1].results.map(dev => dev.mac.toLowerCase()));
+  }
+
+  res.send(macs);
 });
 
 app.get("/printer", async (_, res) => {

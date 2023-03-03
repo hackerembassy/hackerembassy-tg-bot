16b057a50df28870d58a14922d35236d9f9dae24
tectonick <korn9509@gmail.com>
2023-03-03 20:46:52 +0400

    * fixed bug with unlock

--- utils/security.js
+++ utils/security.js
@@ -1,15 +1,15 @@
 const fs = require("fs").promises;
 const NodeRSA = require('node-rsa');
 
-async function encrypt(){
+async function encrypt(message){
     let key = new NodeRSA(await fs.readFile("./sec/pub.key", 'utf8'));
-
-    return key.encrypt(process.env["UNLOCKKEY"], "base64");
+    
+    return key.encrypt(message, "base64");
 }
 
-async function decrypt(){
+async function decrypt(message){
     let key = new NodeRSA(await fs.readFile("./sec/priv.key", 'utf8'));
-    let decryptedKey = key.decrypt(req.body.token);
+    let decryptedKey = key.decrypt(message);
 
     return decryptedKey.toString("utf8");
 }

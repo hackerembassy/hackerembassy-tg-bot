4c4e9a9209070416f3044e1d0d5534e8de2efb05
Himura Kazuto <glagol15@gmail.com>
2023-06-10 22:31:08 +0400

    fix

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -xe
-if [ ! -f "/app/data/db/data.db)" ]; then
-    mkdir -p /app/data/db/
+if [ ! -f "/app/data/db/data.db" ]; then
     cp /app/data/sample.db /app/data/db/data.db
 fi
 exec "$@"

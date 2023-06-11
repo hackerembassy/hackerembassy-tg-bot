f4ce6149ead07482ff82b5f510e7e6cf6373fa64
Himura Kazuto <glagol15@gmail.com>
2023-06-11 12:46:14 +0400

    use config from container

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -3,4 +3,7 @@ set -xe
 if [ ! -f "/app/data/db/data.db" ]; then
     cp /app/data/sample.db /app/data/db/data.db
 fi
+if [ -f "/app/config/sec/production.json" ]; then
+    mv /app/config/sec/production.json  /app/config/production.json
+fi
 exec "$@"

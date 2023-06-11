2ae5f67fedcb1ea1fb2381671452b25a72679392
Himura Kazuto <glagol15@gmail.com>
2023-06-11 14:02:08 +0400

    try to fix tailscale in docker

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -xe
 if [ ! -f "/app/data/db/data.db" ]; then
-    cp /app/data/sample.db /app/data/db/data.db
+    cp -v /app/data/sample.db /app/data/db/data.db
 fi
 if [ -f "/app/config/sec/production.json" ]; then
-    mv /app/config/sec/production.json  /app/config/production.json
+    cp -v /app/config/sec/production.json  /app/config/production.json
 fi
 exec "$@"

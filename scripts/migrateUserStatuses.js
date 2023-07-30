d684378d082a4dc21576fe7bcd737557d593b471
Nickolay Kiselyov <n.kiselyov@itransition.com>
2023-07-30 22:02:31 +0400

    userstatus username update script

---
+++ scripts/migrateUserStatuses.js
@@ -0,0 +1,28 @@
+const statusRepository = require("../repositories/statusRepository");
+
+function migrateUserStatuses(from, to) {
+    console.log("Migrating user statuses usernames");
+    console.log("=======================");
+    if (!from || !to) {
+        console.log("Please provide from and to usernames");
+        return;
+    }
+    console.log("Migrating from: ", from);
+    console.log("Migrating to: ", to);
+    console.log("=======================");
+
+    const userStatuses = statusRepository.getUserStates(from);
+    for (const userStatus of userStatuses) {
+        statusRepository.updateUserState({ ...userStatus, username: to });
+        console.log(`Migrated user status ${userStatus.id} from ${from} to ${to}`);
+    }
+
+    const userStates = statusRepository.getUserStates(from);
+
+    for (const userState of userStates) {
+        statusRepository.updateUserState({ username: to, ...userState });
+        console.log(`Migrated user state ${userState.id} from ${from} to ${to}`);
+    }
+}
+
+migrateUserStatuses(process.argv[2], process.argv[3]);

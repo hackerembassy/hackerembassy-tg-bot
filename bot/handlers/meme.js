4e7adb03d99ec3801955d067d9a73d901d3b7a65
Nickolay Kiselyov <n.kiselyov@itransition.com>
2023-08-10 00:19:11 +0400

    memes handler refactoring

--- bot/handlers/meme.js
+++ bot/handlers/meme.js
@@ -1,20 +1,14 @@
 const { getRandomImageFromFolder } = require("../../services/media");
 
 class MemeHandlers {
-    static randomDogHandler = async (bot, msg) => {
-        let fileBuffer = await getRandomImageFromFolder("./resources/images/dogs");
-        await bot.sendPhoto(msg.chat.id, fileBuffer);
-    };
+    static randomDogHandler = async (bot, msg) =>
+        await bot.sendPhoto(msg.chat.id, await getRandomImageFromFolder("./resources/images/dogs"));
 
-    static randomCatHandler = async (bot, msg) => {
-        let fileBuffer = await getRandomImageFromFolder("./resources/images/cats");
-        await bot.sendPhoto(msg.chat.id, fileBuffer);
-    };
+    static randomCatHandler = async (bot, msg) =>
+        await bot.sendPhoto(msg.chat.id, await getRandomImageFromFolder("./resources/images/cats"));
 
-    static randomCabHandler = async (bot, msg) => {
-        let fileBuffer = await getRandomImageFromFolder("./resources/images/cab");
-        await bot.sendPhoto(msg.chat.id, fileBuffer);
-    };
+    static randomCabHandler = async (bot, msg) =>
+        await bot.sendPhoto(msg.chat.id, await getRandomImageFromFolder("./resources/images/cab"));
 }
 
 module.exports = MemeHandlers;

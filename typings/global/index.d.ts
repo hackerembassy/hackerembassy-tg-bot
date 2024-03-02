cdbf38efc2de06f1b653ca2189a7f591fd5a770c
Nickolay Kiselyov <korn9509@gmail.com>
2024-03-03 03:48:34 +0400

    ghost autoinside

--- typings/global/index.d.ts
+++ typings/global/index.d.ts
@@ -2,6 +2,7 @@ declare global {
     type Optional<T> = T | undefined | null;
     type Nullable<T> = T | null;
     type AnyFunction = (...params: any[]) => any;
+    type MakeRequired<T, Keys extends keyof T> = Omit<T, Keys> & Required<Pick<T, Keys>>;
 }
 
 export {};

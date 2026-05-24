829917e5f4c4ae13d8b83f2ee9b63059be5c5b9d
Mikalai Kisialiou <korn9509@gmail.com>
2026-05-24 13:28:38 +0400

    Recover db path for init

--- data/db.ts
+++ data/db.ts
@@ -1,5 +1,9 @@
+import path from "node:path";
+
+import { PROJECT_ROOT } from "@utils/filesystem";
+
 import { getOrCreateDb } from "./scripts";
 
-const db = getOrCreateDb();
+const db = getOrCreateDb(true, path.join(PROJECT_ROOT, "db/data.db"));
 
 export default db;
